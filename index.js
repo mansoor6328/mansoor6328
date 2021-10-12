eb78a5a9f63c956275233d03be5a1f321272be3d
Yagnesh Modh <yagnesh.modh@gmail.com>
2021-10-12 13:47:25 +0530

    commit code for basic project setup

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { a, b } from "./app";
 
+const abc = "hello";
+
 console.log(a);
 console.log(b);
+console.log(abc);
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "buldle"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
