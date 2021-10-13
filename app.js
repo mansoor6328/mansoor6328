2904cdd0205343ad8675aeb2d9dc50f27ba437a3
Yagnesh Modh <yagnesh.modh@gmail.com>
2021-10-13 09:31:46 +0530

    commit react js setup

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 export const a = 1;
 
 export const b = 2;
+
+const c = 3;
+
+//  only 1 default you can export from one file
+export default c;
---
+++ styles.js
@@ -0,0 +1,8 @@
+const styles = {
+  header: {
+    color: "red",
+    fontSize: 30,
+  },
+};
+
+export default styles;
