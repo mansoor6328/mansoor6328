db65e1cb465564bca3bac2004ad52ddf7158a494
Yagnesh Modh <yagnesh.modh@gmail.com>
2021-11-16 10:49:25 +0530

    commit code for add to cart

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,4 +1,5 @@
 import {
+  Button,
   Card,
   CardContent,
   CardMedia,
@@ -9,19 +10,27 @@ import {
 import { Box } from '@mui/system';
 import I18n from 'i18n-js';
 import React, { Component } from 'react';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import DeleteIcon from '@mui/icons-material/Delete';
 import axiosInstance from '../../utils/axiosInstance';
 
 export default class Home extends Component {
   state = {
+    cart: [],
     products: [],
     error: null,
   };
 
   async componentDidMount() {
     try {
-      const products = await axiosInstance.get('660/products');
+      const res = await Promise.all([
+        axiosInstance.get('660/products'), // 1sec
+        axiosInstance.get('660/cart'), // 1sec
+      ]); // 1 sec
       this.setState({
-        products,
+        products: res[0],
+        cart: res[1],
       });
     } catch (error) {
       this.setState({
@@ -29,6 +38,48 @@ export default class Home extends Component {
       });
     }
   }
+
+  addToCart = async (item) => {
+    try {
+      const res = await axiosInstance.post('660/cart', {
+        ...item,
+        quantity: 1,
+      });
+      this.setState(({ cart }) => ({
+        cart: [...cart, res],
+      }));
+    } catch (error) {
+      this.setState({ error });
+    }
+  };
+
+  updateCart = async (item) => {
+    try {
+      const res = await axiosInstance.put(`660/cart/${item.id}`, item);
+      this.setState(({ cart }) => {
+        const index = cart.findIndex((x) => x.id === item.id);
+        return {
+          cart: [...cart.slice(0, index), res, ...cart.slice(index + 1)],
+        };
+      });
+    } catch (error) {
+      this.setState({ error });
+    }
+  };
+
+  deleteItem = async (item) => {
+    try {
+      await axiosInstance.delete(`660/cart/${item.id}`);
+      this.setState(({ cart }) => {
+        const index = cart.findIndex((x) => x.id === item.id);
+        return {
+          cart: [...cart.slice(0, index), ...cart.slice(index + 1)],
+        };
+      });
+    } catch (error) {
+      this.setState({ error });
+    }
+  };
 
   render() {
     const { products, error } = this.state;
@@ -39,58 +90,123 @@ export default class Home extends Component {
 
     return (
       <div>
-        {products.map((item) => (
-          <Card sx={{ display: 'flex', margin: 2, height: 200 }}>
-            <CardMedia
-              component="img"
-              sx={{ width: 151 }}
-              image={item.image}
-              alt={item.title}
-            />
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-              <CardContent sx={{ flex: '1 0 auto' }}>
-                <Box
-                  component="div"
-                  overflow="hidden"
-                  whiteSpace="pre-line"
-                  textOverflow="ellipsis"
-                  height={30}
-                >
-                  <Typography component="div" variant="h5">
-                    {item.title}
-                  </Typography>
-                </Box>
-                <Box
-                  component="div"
-                  overflow="hidden"
-                  whiteSpace="pre-line"
-                  textOverflow="ellipsis"
-                  height={60}
-                >
+        {products.map((item) => {
+          const cartItem = this.state.cart.find((x) => x.id === item.id);
+          return (
+            <Card sx={{ display: 'flex', margin: 2, minHeight: 200 }}>
+              <CardMedia
+                component="img"
+                sx={{ width: 151 }}
+                image={item.image}
+                alt={item.title}
+              />
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <CardContent sx={{ flex: '1 0 auto' }}>
+                  <Box
+                    component="div"
+                    overflow="hidden"
+                    whiteSpace="pre-line"
+                    textOverflow="ellipsis"
+                    height={30}
+                  >
+                    <Typography component="div" variant="h5">
+                      {item.title}
+                    </Typography>
+                  </Box>
+                  <Box
+                    component="div"
+                    overflow="hidden"
+                    whiteSpace="pre-line"
+                    textOverflow="ellipsis"
+                    height={60}
+                  >
+                    <Typography
+                      sx={{
+                        overflow: 'hidden',
+                      }}
+                      variant="subtitle1"
+                      color="text.secondary"
+                      component="div"
+                    >
+                      {item.description}
+                    </Typography>
+                  </Box>
+                  <Rating
+                    name="read-only"
+                    value={item.rating.rate}
+                    precision={0.5}
+                    readOnly
+                  />
                   <Typography
-                    sx={{
-                      overflow: 'hidden',
-                    }}
-                    variant="subtitle1"
+                    variant="h6"
                     color="text.secondary"
                     component="div"
                   >
-                    {item.description}
+                    {I18n.toCurrency(item.price)}
                   </Typography>
-                </Box>
-                <Rating
-                  name="read-only"
-                  value={item.rating.rate}
-                  precision={0.5}
-                  readOnly
-                />
-                <Typography variant="h6" color="text.secondary" component="div">
-                  {I18n.toCurrency(item.price)}
-                </Typography>
-              </CardContent>
-            </Box>
-          </Card>
-        ))}
+                  {cartItem && (
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                      {cartItem.quantity <= 1 ? (
+                        <IconButton
+                          size="large"
+                          edge="start"
+                          color="inherit"
+                          aria-label="menu"
+                          sx={{ mr: 2 }}
+                          onClick={() => this.deleteItem(cartItem)}
+                        >
+                          <DeleteIcon />
+                        </IconButton>
+                      ) : (
+                        <IconButton
+                          size="large"
+                          edge="start"
+                          color="inherit"
+                          aria-label="menu"
+                          sx={{ mr: 2 }}
+                          onClick={() =>
+                            this.updateCart({
+                              ...cartItem,
+                              quantity: cartItem.quantity - 1,
+                            })
+                          }
+                        >
+                          <RemoveIcon />
+                        </IconButton>
+                      )}
+                      <Typography component="div" variant="h5">
+                        {cartItem.quantity}
+                      </Typography>
+                      <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ ml: 2 }}
+                        onClick={() =>
+                          this.updateCart({
+                            ...cartItem,
+                            quantity: cartItem.quantity + 1,
+                          })
+                        }
+                      >
+                        <AddIcon />
+                      </IconButton>
+                    </Box>
+                  )}
+                  {!cartItem && (
+                    <Button
+                      variant="contained"
+                      onClick={() => this.addToCart(item)}
+                    >
+                      Add To Cart
+                    </Button>
+                  )}
+                </CardContent>
+              </Box>
+            </Card>
+          );
+        })}
       </div>
     );
   }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,28 +1,62 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+  AppBar,
+  Badge,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from '@mui/material';
 import './headerStyle.css';
-import MenuIcon from '../../assets/icons/menu.svg';
+import MenuIcon from '@mui/icons-material/Menu';
+import LogoutIcon from '@mui/icons-material/Logout';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const Header = ({ routes }) => (
-  <header>
-    <div>
-      <div className="logo">
-        <span>Yagnesh</span>
-      </div>
-      {/* <MenuIcon height={24} width={24} fill="red" /> */}
-      <nav>
-        <ul>
-          {routes
-            .filter((x) => x.isAuthRequired)
-            .map((x) => (
-              <li key={x.path}>
-                <Link to={x.path}>{x.title}</Link>
-              </li>
-            ))}
-        </ul>
-      </nav>
-    </div>
-  </header>
+  <AppBar position="static">
+    <Toolbar>
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        Products
+      </Typography>
+      {routes
+        .filter((x) => x.isAuthRequired)
+        .map((x) => (
+          <Button key={x.name} color="inherit">
+            {x.title}
+          </Button>
+        ))}
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+      >
+        <Badge badgeContent={0} color="error">
+          <ShoppingCartIcon />
+        </Badge>
+      </IconButton>
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+      >
+        <LogoutIcon />
+      </IconButton>
+    </Toolbar>
+  </AppBar>
 );
 
 export default Header;
